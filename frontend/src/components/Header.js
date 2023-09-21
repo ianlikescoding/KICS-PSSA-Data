7b5230e4ced4db34b29a096b7d2c72ce6172c3c1
koyasuk <koyasuk@lafayette.edu>
2023-09-21 16:57:41 -0400

    added logo but it does not go to left top:(

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,11 @@
+import "./Header.css"
+
 const Header = () => {
     return (
-        <h1>Header</h1>
+        <div>
+            <img src="./logo.png" className="logo"></img>
+        </div>
+        
     )
 }
 
