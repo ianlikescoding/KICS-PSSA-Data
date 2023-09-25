ccf7a8eb814d66257605e90f0a17538ebfccf040
koyasuk <koyasuk@lafayette.edu>
2023-09-24 21:24:56 -0400

    added dropdown menus

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import { GeneratePieGraph } from './generatePieGraph';
 import Header from './components/Header';
+import CustomizedMenus from './components/DropDown'
 import { generateBarChart } from './generateBarChart';
-
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <CustomizedMenus/>
       <div id='bar_chart1'></div>
-      <button onClick={() => generateBarChart('bar_chart1')} ><span>Generate Chart</span><i></i></button>
-      <button onClick={() => GeneratePieGraph('pie_chart_1')}><span>Button</span><i></i></button>
+      <button onClick={() => generateBarChart('bar_chart1')}> Generate Chart</button>
+      <button onClick={() => GeneratePieGraph('pie_chart_1')}>Click Me</button>
     </div>
   );
 }
