dbb1fd1ce2334f1b23e5ab8729666f6aee3090f8
koyasuk <koyasuk@lafayette.edu>
2023-10-01 21:44:52 -0400

    yay the chart will be displayed in a grid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,27 +5,27 @@ import { generatePieGraph, generateBarChart } from "./generateChart";
 import ComplexedGrid from "./components/ChartContainer";
 
 function App() {
-  let allCharts = [];
-  const [data, setData] = useState();
+  // let allCharts = [];
+  // const [data, setData] = useState();
 
-  setTimeout(() => {
-    setData("Make API call here");
-  }, 1000);
+  // setTimeout(() => {
+  //   setData("Make API call here");
+  // }, 1000);
 
-  useEffect(() => {
-    if (document.getElementById("bar_chart1")) {
-      generateBarChart(allCharts, "bar_chart1");
-    }
+  // useEffect(() => {
+  //   if (document.getElementById("bar_chart1")) {
+  //     generateBarChart(allCharts, "bar_chart1");
+  //   }
 
-    if (document.getElementById("pie_chart1")) {
-      generatePieGraph(allCharts, "pie_chart1");
-    }
-  }, [data]);
+  //   if (document.getElementById("pie_chart1")) {
+  //     generatePieGraph(allCharts, "pie_chart1");
+  //   }
+  // }, [data]);
 
   return (
     <div className="App">
       <Header />
-      <ComplexedGrid allCharts={allCharts}/>
+      <ComplexedGrid />
       
     </div>
   );
--- frontend/src/components/ChartContainer.js
+++ frontend/src/components/ChartContainer.js
@@ -6,9 +6,55 @@ import "../components/ChartContainer.css";
 import Paper from "@mui/material/Paper";
 import { Container } from "@mui/material";
 import { useState, useEffect } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
-export default function ComplexedGrid(allCharts) {
-  const [data, setData] = useState(); // will be used later
+export default function ComplexedGrid() {
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+    },
+  ];
   return (
     <Container>
       <Paper
@@ -36,10 +82,18 @@ export default function ComplexedGrid(allCharts) {
             <Grid item xs container direction="column" spacing={2}>
               <Container>
                 <Grid item xs className="chart-container">
-                <div id="bar_chart1"></div>
+                  <BarChart width={730} height={250} data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="pv" fill="#8884d8" />
+                    <Bar dataKey="uv" fill="#82ca9d" />
+                  </BarChart>
                 </Grid>
                 <Grid item xs className="chart-container">
-                <div id="pie_chart1"></div>
+                  <div id="pie_chart1"></div>
                 </Grid>
               </Container>
               <Grid>PSSA</Grid> {/*horizontal axis label */}
